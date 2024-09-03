9399bcb1604cea1a100b6341d8183b4d6b9ec964
charlie275691502 <charlie275691502@gmail.com>
2024-09-03 12:18:39 +1000

    set automata as index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React, { useState, useRef, useEffect } from "react";
-import { useAppDispatch } from "./app/hooks";
-import { Main } from "./layout/Main";
-
+import { GameController as AutomataController } from "./games/automata/controller";
+import { MainNavBar } from "./components/Nav";
 import "./App.css";
 
 function App() {
-  return <Main></Main>;
+  return (
+    <>
+      <MainNavBar
+        currency={0}
+        handleRestart={() => {
+          /* */
+        }}
+        showNavBar={false}
+      />
+      <AutomataController />
+    </>
+  );
 }
 
 export default App;
