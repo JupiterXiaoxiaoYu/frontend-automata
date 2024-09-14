2bd6cfcce5336d2f27ffdddcdfa6b79d09509dcf
charlie275691502 <charlie275691502@gmail.com>
2024-09-14 20:59:08 +1000

    withdraw entry

--- src/games/components/TopMenu.tsx
+++ src/games/components/TopMenu.tsx
@@ -3,13 +3,29 @@ import "./TopMenu.css";
 import AccountInfo from "./AccountInfo";
 import Resources from "./Resources";
 import Attributes from "./Attributes";
+import WithDrawButton from "./Buttons/WithdrawButton";
+import DepositButton from "./Buttons/DepositButton";
 
 const TopMenu = () => {
+  const onClickWithdraw = () => {
+    /* */
+  };
+
+  const onClickDeposit = () => {
+    /* */
+  };
+
   return (
     <div className="top">
       <div className="top-left"></div>
       <div className="top-middle"></div>
       <div className="top-right"></div>
+      <div className="top-withdraw">
+        <WithDrawButton onClick={onClickWithdraw} />
+      </div>
+      <div className="top-deposit">
+        <DepositButton onClick={onClickDeposit} />
+      </div>
       <AccountInfo />
       <Resources />
       <Attributes />
