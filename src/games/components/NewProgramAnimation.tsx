7a092c7b7f3eef34501d8840ea8504dc74dd5266
charlie275691502 <charlie275691502@gmail.com>
2024-09-23 17:39:10 +1000

    add spinning and adjust timing

--- src/games/components/NewProgramAnimation.tsx
+++ src/games/components/NewProgramAnimation.tsx
@@ -37,53 +37,14 @@ import image_27 from "../images/Animations/NewPrograms/card_27.png";
 import image_28 from "../images/Animations/NewPrograms/card_28.png";
 import image_29 from "../images/Animations/NewPrograms/card_29.png";
 import image_30 from "../images/Animations/NewPrograms/card_30.png";
-import image_31 from "../images/Animations/NewPrograms/card_31.png";
-import image_32 from "../images/Animations/NewPrograms/card_32.png";
-import image_33 from "../images/Animations/NewPrograms/card_33.png";
-import image_34 from "../images/Animations/NewPrograms/card_34.png";
-import image_35 from "../images/Animations/NewPrograms/card_35.png";
-import image_36 from "../images/Animations/NewPrograms/card_36.png";
-import image_37 from "../images/Animations/NewPrograms/card_37.png";
-import image_38 from "../images/Animations/NewPrograms/card_38.png";
-import image_39 from "../images/Animations/NewPrograms/card_39.png";
-import image_40 from "../images/Animations/NewPrograms/card_40.png";
-import image_41 from "../images/Animations/NewPrograms/card_41.png";
-import image_42 from "../images/Animations/NewPrograms/card_42.png";
-import image_43 from "../images/Animations/NewPrograms/card_43.png";
-import image_44 from "../images/Animations/NewPrograms/card_44.png";
-import image_45 from "../images/Animations/NewPrograms/card_45.png";
-import image_46 from "../images/Animations/NewPrograms/card_46.png";
-import image_47 from "../images/Animations/NewPrograms/card_47.png";
-import image_48 from "../images/Animations/NewPrograms/card_48.png";
-import image_49 from "../images/Animations/NewPrograms/card_49.png";
-import image_50 from "../images/Animations/NewPrograms/card_50.png";
-import image_51 from "../images/Animations/NewPrograms/card_51.png";
-import image_52 from "../images/Animations/NewPrograms/card_52.png";
-import image_53 from "../images/Animations/NewPrograms/card_53.png";
-import image_54 from "../images/Animations/NewPrograms/card_54.png";
-import image_55 from "../images/Animations/NewPrograms/card_55.png";
-import image_56 from "../images/Animations/NewPrograms/card_56.png";
-import image_57 from "../images/Animations/NewPrograms/card_57.png";
-import image_58 from "../images/Animations/NewPrograms/card_58.png";
-import image_59 from "../images/Animations/NewPrograms/card_59.png";
-import image_60 from "../images/Animations/NewPrograms/card_60.png";
-import image_61 from "../images/Animations/NewPrograms/card_61.png";
-import image_62 from "../images/Animations/NewPrograms/card_62.png";
-import image_63 from "../images/Animations/NewPrograms/card_63.png";
-import image_64 from "../images/Animations/NewPrograms/card_64.png";
-import image_65 from "../images/Animations/NewPrograms/card_65.png";
-import image_66 from "../images/Animations/NewPrograms/card_66.png";
-import image_67 from "../images/Animations/NewPrograms/card_67.png";
-import image_68 from "../images/Animations/NewPrograms/card_68.png";
-import image_69 from "../images/Animations/NewPrograms/card_69.png";
-import image_70 from "../images/Animations/NewPrograms/card_70.png";
-import image_71 from "../images/Animations/NewPrograms/card_71.png";
+import bg_spin from "../images/backgrounds/bg_spin.png";
 import "./NewProgramAnimation.css";
 import ClaimButton from "./Buttons/ClaimButton";
 import Grid from "./Grid";
 import ProgramResourceDisplay from "./ProgramResourceDisplay";
 import { getResourceIconPath } from "../../data/automata/models";
 import { selectAllPrograms } from "../../data/automata/programs";
+
 import Program from "./Program";
 
 const NewProgramAnimation = () => {
@@ -119,47 +80,6 @@ const NewProgramAnimation = () => {
     image_28,
     image_29,
     image_30,
-    image_31,
-    image_32,
-    image_33,
-    image_34,
-    image_35,
-    image_36,
-    image_37,
-    image_38,
-    image_39,
-    image_40,
-    image_41,
-    image_42,
-    image_43,
-    image_44,
-    image_45,
-    image_46,
-    image_47,
-    image_48,
-    image_49,
-    image_50,
-    image_51,
-    image_52,
-    image_53,
-    image_54,
-    image_55,
-    image_56,
-    image_57,
-    image_58,
-    image_59,
-    image_60,
-    image_61,
-    image_62,
-    image_63,
-    image_64,
-    image_65,
-    image_66,
-    image_67,
-    image_68,
-    image_69,
-    image_70,
-    image_71,
   ];
 
   const dispatch = useAppDispatch();
@@ -200,7 +120,7 @@ const NewProgramAnimation = () => {
       setShowAnimation(true);
       setTimeout(() => {
         setAnimationEnd(true);
-      }, 4000);
+      }, 2000);
     }
   }, [uIState]);
 
@@ -224,6 +144,10 @@ const NewProgramAnimation = () => {
             <div className="new-program-animation-main-animation" />
             {animationEnd && (
               <>
+                <img
+                  src={bg_spin}
+                  className="new-program-animation-spin-image"
+                />
                 <div className="new-program-animation-claim-button">
                   <ClaimButton onClick={onClickClaim} />
                 </div>
