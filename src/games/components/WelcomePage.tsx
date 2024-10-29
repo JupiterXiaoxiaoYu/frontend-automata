8cf090da8fd9bd0530907026b9e6eafb6df6dac2
charlie275691502 <charlie275691502@gmail.com>
2024-10-29 18:32:57 +1100

    Add sponsor icon

--- src/games/components/WelcomePage.tsx
+++ src/games/components/WelcomePage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./WelcomePage.css";
 import PlayButton from "./Buttons/PlayButton";
 import WelcomePageProgressBar from "./WelcomePageProgressBar";
+import logo from "../images/backgrounds/sponsor logo.png";
 
 interface Props {
   progress: number;
@@ -13,6 +14,7 @@ const WelcomePage = ({ progress, message, onClick }: Props) => {
   return (
     <div className="welcome-page-container">
       <div className="welcome-page-background"></div>
+      <img src={logo} className="welcome-page-logo-image" />
       <div className="welcome-page-play-button">
         <PlayButton onClick={onClick} />
       </div>
