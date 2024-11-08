2cc44dc0066d8e89343e1da466f38c53370cf71b
charlie275691502 <charlie275691502@gmail.com>
2024-11-08 22:08:11 +1100

    update guide layout

--- src/games/components/Guides/GuideCircle.tsx
+++ src/games/components/Guides/GuideCircle.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import image from "../../images/Guides/circle.png";
+import arrowImage from "../../images/Guides/circle_arrow.png";
 import "./GuideCircle.css";
 
 const GuideCircle = () => {
   return (
     <div>
       <img src={image} className="guide-popup-circle-image" />
+      <img src={arrowImage} className="guide-popup-circle-arrow-image" />
       <p className="guide-popup-circle-content-title">4. Assembly Panel</p>
       <p className="guide-popup-circle-content-text">
         Choose 8 Programs for an automaton from the Program Board on the right.
--- src/games/components/Guides/GuideProgram.tsx
+++ src/games/components/Guides/GuideProgram.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import image from "../../images/Guides/resource.png";
+import image from "../../images/Guides/program.png";
+import arrowImage from "../../images/Guides/program_arrow.png";
 import "./GuideProgram.css";
 
 const GuideProgram = () => {
   return (
     <div>
       <img src={image} className="guide-popup-program-image" />
+      <img src={arrowImage} className="guide-popup-program-arrow-image" />
       <p className="guide-popup-program-content-title">3. Program List</p>
       <p className="guide-popup-program-content-text">
         You can purchase new programs using Titanium and participate in our
